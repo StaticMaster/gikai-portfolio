c1981b85d54732678ead6f6e397dd99de3d785f5
Gikai Andrews <gnaboys@gmail.com>
2023-12-27 14:59:03 -0500

    added dark mode transition to global css and footer

--- portfolio/src/Footer/Footer.js
+++ portfolio/src/Footer/Footer.js
@@ -1,9 +1,12 @@
 import React from "react";
 import "./Footer.css";
+import { useDarkMode } from "../DarkModeContext";
 
 function Footer() {
+  const { isDarkMode } = useDarkMode();
+
   return (
-    <div className="footer-section">
+    <div className={`footer-section ${isDarkMode ? "dark-mode" : ""}`}>
       <p className="footer-text">© made by Gikai Andrews</p>
     </div>
   );
--- portfolio/src/NavMenu/NavMenu.js
+++ portfolio/src/NavMenu/NavMenu.js
@@ -1,9 +1,5 @@
 import React, { useState } from "react";
 import "./NavMenu.css";
-// import { FaHouse } from "react-icons/fa6";
-// import { GrProjects } from "react-icons/gr";
-// import { IoCodeSlashOutline } from "react-icons/io5";
-// import { IoMdPerson } from "react-icons/io";
 import { useDarkMode } from "../DarkModeContext";
 
 const Navbar = () => {
@@ -17,8 +13,6 @@ const Navbar = () => {
 
   return (
     <nav className={isDarkMode ? 'navbar dark-mode': 'navbar'}>
-      {/* <input type="checkbox" id="darkmode-toggle" checked={isDarkMode} onChange={toggleDarkMode}/>
-      <label htmlFor="darkmode-toggle"></label> */}
       <div
         className={`menu-toggle ${isOpen ? "open" : ""}`}
         onClick={toggleMenu}
@@ -29,19 +23,15 @@ const Navbar = () => {
       </div>
       <div className={`navbar-links ${isOpen ? "open" : ""}`}>
         <div className="navbar-title">
-          {/* <FaHouse className="navbar-icon" /> */}
           <a href="#home">Home</a>
         </div>
         <div className="navbar-title">
-          {/* <GrProjects className="navbar-icon" /> */}
           <a href="#projects">Projects</a>
         </div>
         <div className="navbar-title">
-          {/* <IoCodeSlashOutline className="navbar-icon" /> */}
           <a href="#skills">Skills</a>
         </div>
         <div className="navbar-title">
-          {/* <IoMdPerson className="navbar-icon" /> */}
           <a href="#about">About Me</a>
         </div>
         <input type="checkbox" id="darkmode-toggle" checked={isDarkMode} onChange={toggleDarkMode}/>
